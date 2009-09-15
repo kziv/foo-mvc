3a9beb025d042d458ff11b3a2cca34d2cbc410c2
Karen Ziv <karen@perlessence.com>
2009-09-15 01:58:00 +0000

    Created router class and incorporated into FooMVC::dispatch()

--- foo-mvc.php
+++ foo-mvc.php
@@ -45,7 +45,7 @@ class FooMVC {
      * Main dispatcher
      **/
     public static function dispatch() {
-
+        
         self::$base_path       = dirname(__FILE__);
         self::$dir_controllers = self::$base_path . '/controllers/';
         self::$dir_models      = self::$base_path . '/models/';
@@ -57,10 +57,10 @@ class FooMVC {
         if (self::$base_url) {
             $path = substr($_SERVER['REQUEST_URI'], strlen(self::$base_url));
         }
-        $path = explode('/', $path);           // Split on the slash
-        $path = array_filter($path, 'strlen'); // Remove empty entries
-        
-        $controller_name = implode('_', $path);
+        $router = new Router;
+        $controller_name = $router->url2controller($path);
+        print_h($controller_name);
+        $controller_name = str_replace('.', '_', $controller_name);
         self::forward($controller_name);
 
         self::runView();
@@ -253,3 +253,174 @@ abstract class FooMVCView {
     protected $data = array(); // Data packet to send to template
     
 }
+
+class Router {
+
+    protected $routes = array();
+    protected $urlparams = array();
+    
+    public function __construct($ini_path=NULL) {
+        // Set the default routes path
+        if (!$ini_path) {
+            $ini_path = dirname(__FILE__) . '/routes.ini';
+        }
+        $this->loadRoutesFile($ini_path);
+    }
+
+    /**
+     * Loads a router file and parses the route map for reading
+     * @param  string Path (absolute or relative) to routes file
+     * @throws RouterException if file isn't found
+     **/
+    protected function loadRoutesFile($ini_path) {
+
+        // Get the raw routes from the ini file
+        if (!file_exists($ini_path)) {
+            throw new RouterException("Router file expected at '$ini_path'");
+        }
+        $routes = parse_ini_file($ini_path);
+        print_h($routes); // DEBUG
+        
+        // For each route...
+        foreach ($routes as $raw_route => $controller) {
+
+            if (!$raw_route) {
+                $this->routes['controller'] = $controller;
+                return;
+            }
+            
+            $cur_route = &$this->routes;
+            
+            // Traverse the route and parse into a searchable tree
+            // We do this instead of a regex because there could
+            // potentially be hundreds of maps with var replacement
+            // and regexes are heavy processing. This map can be cached.
+            $raw_tokens = explode('/', $raw_route);
+            foreach ($raw_tokens as $i => $token) {
+                
+                // Are we on the last token in the URL?
+                $is_last_token = $i == (count($raw_tokens) - 1);
+
+                // If the current route doesn't have a subcontrollers section, create and traverse to it
+                if (!isset($cur_route['sub'])) {
+                    $cur_route['sub'] = array();
+                }
+                $cur_route = &$cur_route['sub'];
+
+                // Determine if the current token is a var or standard
+                // Vars are in format :var
+                if (strpos($token, ':') === FALSE) { // Standard token
+                    $array_token = $token;
+                    $urlparams = array();
+                }
+                else { // Variable token
+                    $array_token = ':';
+                    $urlparams = array('var' => substr($token, 1));
+                }
+
+                if (isset($cur_route[$array_token])) {
+                    // Make sure that all variable tokens in the same location in the same path are named the same
+                    // This prevents multiple routes created for the same replacement location
+                    // (e.g. For route foo/:bar/baz, foo/:bar/quux is correct, but not foo/:var/quux)
+                    if ($array_token == ':' && ($cur_route[$array_token]['var'] != substr($token, 1))) {
+                        trigger_error("ROUTER: Token '" . substr($token, 1) . "' differs from same level variable tokens");
+                        return;
+                    }
+                }
+                else {
+                    $cur_route[$array_token] = $urlparams;
+                }
+
+                // If this is the last token in the URL, assign the controller
+                if ($is_last_token) {
+                    $cur_route[$array_token]['controller'] = $controller;
+                }
+
+                // Traverse one more step down the URL map
+                $cur_route = &$cur_route[$array_token];
+            }
+            
+        }
+
+        print_h($this->routes); // DEBUG
+
+    }
+
+    /**
+     * Finds the controller for a given URL
+     * Also populates the $urlparams variable
+     * @param  string URL to find a controller for
+     * @params bool   If a route isn't found, use the URL as the controller name (e.g. foo/bar => Foo_Bar)
+                      This allows you to use routes.ini only for routes that deviate from that pattern
+     * @return string Controller name
+     **/
+    public function url2controller($url, $path_fallback=TRUE) {
+
+        // Split the URL on slashes and remove empty entries
+        $tokenized_url = explode('/', $url);
+        $tokenized_url = array_filter($tokenized_url, 'strlen');
+        
+        $cur_route = $this->routes; // Set the first current route as the head of the routes map
+
+        // Create the fallback controller
+        $fallback = $path_fallback ? implode('_', $tokenized_url) : FALSE;
+
+        // For each part of the URL...
+        foreach ($tokenized_url as $i => $token) {
+            
+            // If the route map path we're on has subcontrollers...
+            if (isset($cur_route['sub'])) {
+                $cur_route = $cur_route['sub']; // Traverse down into the subcontrollers
+                // Find the subcontroller route to take
+                if (isset($cur_route[$token])) { // Token found
+                    $cur_route = &$cur_route[$token];
+                }
+                elseif (isset($cur_route[':'])) { // Look for vars at the same level
+                    $cur_route = &$cur_route[':'];
+                    $this->urlparams[$cur_route['var']] = $token;
+                }
+                else {
+                    return $fallback;
+                }
+            }
+            else {
+                return $fallback;
+            }
+        }
+        
+        // If we've found a controller...
+        if (isset($cur_route['controller'])) {
+
+            // If there's no vars to replace in the controller name, just return it
+            if (strpos($cur_route['controller'], ':') === FALSE) { 
+                return $cur_route['controller'];
+            }
+
+            // Replace all controller vars with their URL var counterparts, if they exist
+            $controller_tokens = explode('.', $cur_route['controller']);
+            foreach ($controller_tokens as $i => $token) {
+                if (strpos($token, ':') === 0) { // If this token is a controller var
+                    $var = substr($token, 1);
+                    if (isset($urlparams[$var])) {
+                        $controller_tokens[$i] = $urlparams[$var];
+                    }
+                    else {
+                        trigger_error("No matching URL param '$token' in controller '" . $cur_route['controller']);
+                        return $fallback;
+                    }
+                }
+            }
+
+            return implode('.', $controller_tokens);
+
+        }
+        return FALSE;
+        
+    }
+
+    public function controller2url($controller) {
+
+    }
+}
+
+class RouterException extends Exception {}
